09a7c17f6978b5ed81b6890992905e1249aa5ba5
Smit417263 <56094552+Smit417263@users.noreply.github.com>
2020-11-21 23:36:18 -0800

    Update [spate130@cs.ucr.edu]_lab9_part1.c

--- turnin/[spate130@cs.ucr.edu]_lab9_part1.c
+++ turnin/[spate130@cs.ucr.edu]_lab9_part1.c
@@ -7,7 +7,7 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  *	
- *	Demo Link:
+ *	Demo Link: https://youtu.be/X3wrYHZOJFc
 */
 #include <avr/io.h>
 #include <avr/interrupt.h>
